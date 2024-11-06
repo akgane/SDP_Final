f645844e40a672750f7d57efc4e62db3b54ea3ec
kamila <230834@astanait.edu.kz>
2024-11-06 16:00:41 +0500

    Primitive tests for program work in Main were created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import Facade.TaskManagerFacade;
+import Factory.Task;
+import Observer.TeamMember;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TaskManagerFacade facade = new TaskManagerFacade();
+
+        facade.createTask("Bug", "BugFix1");
+        facade.createTask("Feature", "NewFeature1");
+
+        Task task = facade.getTaskByTitle("BugFix1");
+        System.out.println(task.getStatus());
+        facade.updateTaskStatus(task, "In progress");
+        System.out.println(task.getStatus());
+
+        TeamMember member = new TeamMember("Alice", "alice@astanait.edu.kz");
+        facade.addObserverToTask(task, member);
+
+        facade.undoLastAction();
+        facade.displayAllTasks();
     }
 }
